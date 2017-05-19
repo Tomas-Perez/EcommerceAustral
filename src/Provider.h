d54a75fa1185e9531bacb7f8b414771fee939349
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-05-19 17:45:28 -0300

    Added all user skeleton files to use in the database. Added missing files to CMakeLists.

---
+++ src/Provider.h
@@ -0,0 +1,8 @@
+#ifndef ECOMMERCEAUSTRAL_PROVIDER_H
+#define ECOMMERCEAUSTRAL_PROVIDER_H
+
+typedef struct Provider{
+    int userID;
+}Provider;
+
+#endif //ECOMMERCEAUSTRAL_PROVIDER_H
---
+++ src/SupportStaff.h
@@ -0,0 +1,8 @@
+#ifndef ECOMMERCEAUSTRAL_SUPPORTSTAFF_H
+#define ECOMMERCEAUSTRAL_SUPPORTSTAFF_H
+
+typedef struct SupportStaff{
+    int userID;
+}SupportStaff;
+
+#endif //ECOMMERCEAUSTRAL_SUPPORTSTAFF_H
