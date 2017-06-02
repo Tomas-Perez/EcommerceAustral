0fa7f2b8939ff1126c7352b2fe1f1753bfa7fa55
brianfroschauer <brian.froschauer@ing.austral.edu.ar>
2017-06-02 17:08:34 -0300

    Student menu

--- src/Menu/StudentMenu.c
+++ src/Menu/StudentMenu.c
@@ -2,7 +2,151 @@
 #include "../Bank/Bank.h"
 #include "../College/College.h"
 #include "../Ecommerce/SupportDatabase.h"
+#include "StudentMenu.h"
+#include "../Util/ScanUtil.h"
+#include <stdio.h>
 
 void studentMenu(UserDatabase* database, Bank* bank, SupportDatabase* supportDatabase, College* college, int userID){
+    printf("Enter an option: \n");
+    printf("1. Campus \n");
+    printf("2. E-Commerce \n");
+    printf("3. Bank \n");
+    printf("4. Edit information \n");
 
+    int option = scanInt();
+
+    switch (option) {
+        case 1:
+            campusOption(database, college, userID);
+        case 2:
+            eCommerceOption(database, bank);
+        case 3:
+            bankOption(database, bank, userID);
+        case 4:
+            editInformationOption(database);
+        default:
+            printf("Invalid option");
+    }
+}
+
+void campusOption(UserDatabase* database, College* college, int userID) {
+    printf("Enter an option: \n");
+    printf("1. Show books and subjects \n");
+    printf("2. Subscribe to subjects \n");
+
+    int option = scanInt();
+
+    switch (option) {
+        case 1:
+            printf("BOOKS: \n");
+            ArrayOfBooks* books = getBooksOfStudent(college, userID);
+            int i;
+            for (i = 0; i < books->amountOfBooks; i++) {
+                printf("%s+%s", books->books[i]->title, "\n");
+            }
+
+            printf("SUBJECTS: \n");
+            ArrayOfSubjects* subjects = getSubjectsOfStudent(college, userID);
+
+            for (i = 0; i < subjects->amountOfSubjects; i++) {
+                printf("%s+%s", subjects->subjects[i]->name, "\n");
+            }
+
+        case 2:
+            printf("Enter a Subject Id: \n");
+            int subjectID = scanInt();
+            enrollStudentInSubject(college, subjectID, userID);
+
+        default:
+            printf("Invalid option");
+
+    }
+
+}
+void eCommerceOption(UserDatabase* database, Bank* bank) {
+    printf("Enter an option: \n");
+    printf("1. Add or remove books to cart \n");
+    printf("2. Checkout bill \n");
+    printf("3. Shopping history \n");
+    printf("4. Support query \n");
+
+    int option = scanInt();
+
+    switch (option) {
+        case 1:
+            printf("1. Add book");
+            printf("2. Remove book");
+            printf("3. Remove all books.");
+            int option2 = scanInt();
+            switch (option2) {
+                case 1:
+                    // No tengo forma de obtener el cart de cada user
+                    //cartAddBook();
+                case 2:
+                    //cartRemoveBook();
+                case 3:
+                    //cartRemoveAllBooks();
+                default:
+                    printf("Invalid option");
+            }
+        case 2:
+
+        case 3:
+
+        case 4:
+
+        default:
+            printf("Invalid option");
+
+    }
+}
+void bankOption(UserDatabase* database, Bank* bank, int userID) {
+    printf("Enter an option: \n");
+    printf("1. Create account \n");
+    printf("2. Deposit \n");
+    printf("3. Withdraw \n");
+    printf("4. Transaction history \n");
+
+    int option = scanInt();
+
+    switch (option) {
+        case 1:
+            printf("Enter a Bank Account ID: \n");
+            int bankAccountID = userID;
+            //BankAccount* bankAccount = createBankAccount(bankAccountID, 0, 0, userID);
+            //addAccount(bank, bankAccount);
+            printf("Successfully created bank account!");
+        case 2:
+            printf("Enter amount: \n");
+            double amount1 = scanInt();
+            //Transaction* transaction1 = deposit_Money(bank, userID, amount1); // UserId =? bankAccountID
+        case 3:
+            printf("Enter amount: \n");
+            double amount2 = scanInt();
+            //Transaction* transaction2 = withdraw_Money(bank, userID, amount2);
+        case 4:
+            // transactionHistiy(bank, bankAccuntID)
+        default:
+            printf("Invalid option");
+    }
+}
+
+void editInformationOption(UserDatabase* database) {
+    printf("Enter an option: \n");
+    printf("1. Change password \n");
+    printf("2. Change name \n");
+    printf("3. Change phone number \n");
+
+    int option = scanInt();
+
+    switch (option) {
+        case 1:
+
+        case 2:
+
+        case 3:
+
+        default:
+            printf("Invalid option");
+    }
 }
