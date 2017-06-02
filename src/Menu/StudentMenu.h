58a104876a69162376e06d6a1e700a10f19ef5bc
brianfroschauer <brian.froschauer@ing.austral.edu.ar>
2017-06-02 17:10:32 -0300

    Student menu

---
+++ src/Menu/StudentMenu.h
@@ -0,0 +1,20 @@
+//
+// Created by micaeladominguez on 02/06/17.
+//
+
+#ifndef ECOMMERCEAUSTRAL_STUDENTMENU_H
+#define ECOMMERCEAUSTRAL_STUDENTMENU_H
+
+typedef struct stuentMenu StudentMenu;
+
+struct stuentMenu {
+
+};
+
+void studentMenu(UserDatabase* database, Bank* bank, SupportDatabase* supportDatabase, College* college, int userID);
+void campusOption(UserDatabase* database, College* college, int userID);
+void eCommerceOption(UserDatabase* database, Bank* bank);
+void bankOption(UserDatabase* database, Bank* bank, int userID);
+void editInformationOption(UserDatabase* database);
+
+#endif //ECOMMERCEAUSTRAL_STUDENTMENU_H
