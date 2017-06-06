4f9f3ead5f33fc118d087086a0ed18077a3f3c5f
Nicolás Gargano <nicolas.gargano@ing.austral.edu.ar>
2017-06-06 13:34:25 -0300

    Updated admin and provider's documentation

--- src/Ecommerce/Admin.c
+++ src/Ecommerce/Admin.c
@@ -1,7 +1,11 @@
 #include "Admin.h"
 #include <stdlib.h>
 #include <string.h>
-
+/*
+ * Function: createAdmin
+ * Description: allocates memory for an Admin and its components.
+ * Returns: Admin pointer
+ */
 Admin* createAdmin(char* name, int password, int phoneNumber){
     Admin* result = malloc(sizeof(Admin));
     result->name = malloc(sizeof(char)*strlen(name)+1);
@@ -12,6 +16,11 @@ Admin* createAdmin(char* name, int password, int phoneNumber){
     return result;
 }
 
+/*
+ * Function: destroyAdmin
+ * Description: frees all memory of the specified Admin.
+ * Returns: -
+ */
 void destroyAdmin(Admin* admin){
     free(admin->name);
     free(admin);
